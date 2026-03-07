d973e8299f8fc05af506b3c3587d94614f0489ea
felixvanmill <felix@van-mill.nl>
2026-03-07 10:24:29 +0100

    Demo change for pipeline 1

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 // Routes
 app.get('/', (req, res) => {
   res.json({
-    message: 'Welcome to the Les 8 API!',
+    message: 'Welcome to DevOpsEindopdracht',
     version: process.env.APP_VERSION || '1.0.0',
     environment: process.env.NODE_ENV || 'development'
   });
