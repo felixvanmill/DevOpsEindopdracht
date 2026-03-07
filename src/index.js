5d6c83ad01ae29d796bec4e7628f9add842145b8
felixvanmill <felix@van-mill.nl>
2026-03-07 11:19:00 +0100

    kleine code aanpassing voor eindopdracht

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 // Routes
 app.get('/', (req, res) => {
   res.json({
-    message: 'Welcome to DevOpsEindopdracht',
+    message: 'Welcome to DevOpsEindopdracht DemoTest',
     version: process.env.APP_VERSION || '1.0.0',
     environment: process.env.NODE_ENV || 'development'
   });
